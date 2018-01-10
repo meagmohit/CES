8b25d686a13e7ddf829001daeeaf20596d943327
Mohit <me.agmohit@gmail.com>
2018-01-09 17:35:32 -0800

    CSP code works

--- CES1R/input_fast.py
+++ CES1R/input_fast.py
@@ -10,6 +10,7 @@ import os
 import numpy as np
 import tensorflow as tf
 import random
+import pickle
 
 class input(object):
   
@@ -29,24 +30,25 @@ class input(object):
     self._train_total = len(train_files)
     self._test_total = len(test_files)
 
-    data = np.zeros([self._train_total, self._num_electrodes, self._step_size])
-    label = np.zeros([self._train_total, 1])
-    for ind in xrange(self._train_total):
-      filename = self._train_files[ind]
-      filepath_data = os.path.join('data/','train',filename +'.txt')
-      filepath_lab = os.path.join('data/','train',filename +'.lab')
-      data[ind,:,:] = np.transpose(np.loadtxt(filepath_data))
-      f_temp = open(filepath_lab,'r')
-      lab_temp = f_temp.read()
-      f_temp.close()
-      if lab_temp=='Like':
-        label[ind,0]=1
-      elif lab_temp=='Disike':
-        label[ind,0]=2
-      else:
-        print("\033[91m  Error in Labels check .. \033[0m")
-    csp = mne.decoding.CSP(n_components=5, cov_est='epoch', transform_into='csp_space')
-    self._csp = mycsp = csp.fit(data,label)
+    #data = np.zeros([self._train_total, self._num_electrodes, self._step_size])
+    #label = np.zeros([self._train_total, 1])
+    #for ind in xrange(self._train_total):
+    #  filename = self._train_files[ind]
+    #  filepath_data = os.path.join('data/','train',filename +'.txt')
+    #  filepath_lab = os.path.join('data/','train',filename +'.lab')
+    #  data[ind,:,:] = np.transpose(np.loadtxt(filepath_data))
+    #  f_temp = open(filepath_lab,'r')
+    #  lab_temp = f_temp.read()
+    #  f_temp.close()
+    #  if lab_temp=='Like':
+    #    label[ind,0]=1
+    #  elif lab_temp=='Disike':
+    #    label[ind,0]=2
+    #  else:
+    #    print("\033[91m  Error in Labels check .. \033[0m")
+    #csp = mne.decoding.CSP(n_components=5, cov_est='epoch', transform_into='csp_space')
+    file_pi = open('csp.obj', 'r') 
+    self._csp = mycsp = pickle.load(file_pi) #csp.fit(data,label)
     
      
   def next_batch(self, sess, flag):
